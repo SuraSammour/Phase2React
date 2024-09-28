9c6fcedc4a1e54d35d6306a838fecc403887bc1b
SuraSammour <surasammour12@gmail.com>
2024-09-28 10:56:09 +0300

    UPDATE error handling

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,13 +14,17 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
     return (
         <div className="productItem container row">
-            {products.map((product, index) => (
-                <div key={index} className="product">
-                    <h3>Name: {product.name}</h3>
-                    <p>Category: {product.category}</p>
-                    <p>Price: ${product.price}</p>
-                </div>
-            ))}
+            {products.length === 0 ? (
+                <p>No products found</p>
+            ) : (
+                products.map((product, index) => (
+                    <div key={index} className="product">
+                        <h3>Name: {product.name}</h3>
+                        <p>Category: {product.category}</p>
+                        <p>Price: ${product.price}</p>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
